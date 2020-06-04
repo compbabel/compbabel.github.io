d1009b449c10461198bc58a80b010f5ed02424de
compbabel <65569694+compbabel@users.noreply.github.com>
2020-06-03 20:06:34 -0700

    Add files via upload

---
+++ js/js/stackqueuehash.js
@@ -0,0 +1,72 @@
+var hash;
+
+function init_hash() {
+    var sym_cols = '!+-*/()';
+    var sym_rows = '!+-*/(';
+    
+    var table = [[4,1,1,1,1,1,5],
+    [2,2,2,1,1,1,2],
+    [2,2,2,1,1,1,2],
+    [2,2,2,2,2,1,2],
+    [2,2,2,2,2,1,2],
+    [5,1,1,1,1,1,3]]
+
+    hash = new Map();
+    for(var i = 0; i < 6; i++) {
+        var char_row = sym_rows.charAt(i);
+        for (var j = 0; j < 7; j++) {
+            var char_col = sym_cols.charAt(j);
+            var key = char_row + char_col;
+            hash.set(key, table[i][j]);         
+        }
+    }
+}
+
+function get_code(key) {
+    var val = hash.get(key);
+    return val;
+}
+
+class Stack {
+    constructor() {
+        this.arr = [];
+    }
+
+    push(str) {
+        this.arr.push(str);
+    }
+
+    pop() {
+        return this.arr.pop();
+    }
+
+    peek() {
+        return this.arr[this.arr.length-1];
+    }
+  
+    data() {
+        return this.arr;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.arr = [];
+    }
+
+    enq(str) {
+        this.arr.push(str);
+    }
+
+    deq() {
+        return this.arr.shift();
+    }
+
+    peek() {
+        return this.arr[0];
+    }
+
+    data() {
+        return this.arr;
+    }
+}
