8ea7b0b42115f3bc1d5c699e039355dfd1fe215e
compbabel <65569694+compbabel@users.noreply.github.com>
2022-05-03 20:50:15 -0700

    ttt

---
+++ retirement/w1_coding/js/movie.js
@@ -0,0 +1,41 @@
+const elemButton = document.getElementById("suggest");
+const elemTitle = document.getElementById("movie-title");
+const elemRating = document.getElementById("rating");
+const elemStar1 = document.getElementById("star1");
+const elemStar2 = document.getElementById("star2");
+const elemStar3 = document.getElementById("star3");
+let movies;
+
+function suggest() {
+  let index = getRandomInt(movies.items.length);
+  const movie = movies.items[index];
+
+  elemTitle.innerText = movie.fullTitle;
+  elemRating.innerText = movie.imDbRating;
+  let stars = movie.crew.split(",");
+  elemStar1.innerText = stars[0];
+  elemStar2.innerText = stars[1];
+  elemStar3.innerText = stars[2];
+}
+
+async function getMovies() {
+  movies = JSON.parse(localStorage.getItem("imdbMovies"));
+  if (movies != null) {
+    console.log(movies);
+    suggest();
+    return;
+  }
+
+  // alert("getting movie from imdb")
+  let res = await fetch("https://imdb-api.com/en/API/Top250Movies/k_7b5cpl5n");
+  let data = await res.json();
+  movies = data;
+  localStorage.setItem("imdbMovies", JSON.stringify(movies));
+  suggest();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+getMovies()
